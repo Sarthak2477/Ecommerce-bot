77aff8370ddc463c1fa8bc0c71a8bbb4c2d3f105
Sarthak2477 <64775083+Sarthak2477@users.noreply.github.com>
2025-06-11 20:49:00 +0530

    Update groq_utils.py

--- product-server/groq_utils.py
+++ product-server/groq_utils.py
@@ -1,25 +1,27 @@
 from groq import Groq
 import os
 from prompt import LLM_PROMPT
+from dotenv import load_dotenv
 
-client = Groq(api_key="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")
+load_dotenv() 
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+
+
+client = Groq(api_key=GROQ_API_KEY)
 
 def get_sql_from_groq(user_input):
     prompt = LLM_PROMPT.format(input=user_input)
     print(user_input)
     chat_response = client.chat.completions.create(
         messages=[{"role": "user", "content": prompt}],
-        model="llama3-8b-8192"  # Or "mixtral-8x7b-32768"
+        model="llama3-8b-8192"
     )
     
     output = chat_response.choices[0].message.content.strip()
     print(output)
-    # Extract SQL line from response
-    lines = output.splitlines()
-    for line in lines:
+    for line in output.splitlines():
         if line.strip().lower().startswith("select"):
             return line.strip()
-    
     return None
-
-
